2af80718a3aea4c54812a9a9570149e651a0f4ef
Wayne Parker <wayne@wayneparker.biz>
2016-05-27 20:45:34 -0400

    Re-write gulpfile to avoid FSEvent error; still getting error, but not crashing.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 // Imports
 var gulp = require('gulp');
 
+// Styles
+var sass = require('gulp-sass');
+
 // Paths
 var
 	src =  'assets',
@@ -14,16 +17,18 @@ var paths = {
 		root:      src,
 		styles: {
 			sass: [
-				       src + '/styles/**.*.sass',
-				       src + '/styles/**.*.scss'
+				       src + '/styles/**/*.sass',
+				       src + '/styles/**/*.scss',
+				 '!' + src + '/styles/**/*_.sass',
+				 '!' + src + '/styles/**/*_.scss'
 			],
-			less:    src + '/styles/**.*.less'
+			less:    src + '/styles/**/*.less'
 		},
 		css:       src + '/css',
 		scripts: {
-			es6:     src + '/scripts/**.*.es6',
-			js:      src + '/scripts/**.*.js',
-			coffee:  src + '/scripts/**.*.coffee'
+			es6:     src + '/scripts/**/*.es6',
+			js:      src + '/scripts/**/*.js',
+			coffee:  src + '/scripts/**/*.coffee'
 		},
 		js:        src + '/js',
 		images:    src + '/images',
@@ -58,86 +63,23 @@ var paths = {
 
 // Task Definitions
 
-// TODO: Add scripts to compile / concat Scripts and Styles
-
-
 // Push compiled / static assets to Staging
 
 gulp.task('html', function() {
-	return gulp.src(paths.src.root + '/**.*.html')
+	return gulp.src(paths.src.root + '/**/*.html')
 		.pipe(gulp.dest(paths.dev.root));
 });
 
-// TODO: Replace with gulp-minify with sourcemaps
+// push CSS up to staging
 gulp.task('css', function() {
-	return gulp.src(paths.src.css + '/**.*')
+	return gulp.src(paths.src.css + '/**/*.css')
 		.pipe(gulp.dest(paths.dev.css));
 });
 
-// TODO: Replace with gulp-uglify with sourcemaps
+// TODO: Add gulp-uglify with sourcemaps
 gulp.task('js', function() {
-	return gulp.src(paths.src.js + '/**.*')
+	return gulp.src(paths.src.js + '/**/*')
 		.pipe(gulp.dest(paths.dev.js));
-});
-
-gulp.task('img', function() {
-	return gulp.src(paths.src.img + '/**.*')
-		.pipe(gulp.dest(paths.dev.img));
-});
-
-gulp.task('docs', function() {
-	return gulp.src(paths.src.docs + '/**.*')
-		.pipe(gulp.dest(paths.dev.docs));
-});
-
-gulp.task('fonts', function() {
-	return gulp.src(paths.src.fonts + '/**.*')
-		.pipe(gulp.dest(paths.dev.fonts));
-});
-
-gulp.task('media', function() {
-	return gulp.src(paths.src.media + '/**.*')
-		.pipe(gulp.dest(paths.dev.media));
-});
-
-
-// Push to Production for deployment
-
-gulp.task('html', function() {
-	return gulp.src(paths.src.root + '/**.*.html')
-		.pipe(gulp.dest(paths.prod.root));
-});
-
-// TODO: Replace with gulp-minify-css, NO source maps
-gulp.task('css-prod', function() {
-	return gulp.src(paths.src.css + '/**.*')
-		.pipe(gulp.dest(paths.prod.css));
-});
-
-// TODO: Replace with gulp-uglify, NO sourcemaps
-gulp.task('js-prod', function() {
-	return gulp.src(paths.src.js + '/**.*')
-		.pipe(gulp.dest(paths.prod.js));
-});
-
-gulp.task('img-prod', function() {
-	return gulp.src(paths.src.img + '/**.*')
-		.pipe(gulp.dest(paths.prod.img));
-});
-
-gulp.task('docs-prod', function() {
-	return gulp.src(paths.src.docs + '/**.*')
-		.pipe(gulp.dest(paths.prod.docs));
-});
-
-gulp.task('fonts-prod', function() {
-	return gulp.src(paths.src.fonts + '/**.*')
-		.pipe(gulp.dest(paths.prod.fonts));
-});
-
-gulp.task('media-prod', function() {
-	return gulp.src(paths.src.media + '/**.*')
-		.pipe(gulp.dest(paths.prod.media));
 });
 
 
@@ -145,12 +87,11 @@ gulp.task('media-prod', function() {
 //## Watch for Changes
 
 gulp.task('watch', function () {
-	gulp.watch([paths.src.root + '**/*.html'], ['html']);
-	gulp.watch([paths.src.css + '**/*'],       ['css']);
-	gulp.watch([paths.src.js + '**/*'],        ['js']);
-	gulp.watch([paths.src.img + '**/*'],       ['img']);
-	gulp.watch([paths.src.docs + '**/*'],      ['docs']);
-	gulp.watch([paths.src.fonts + '**/*'],     ['fonts']);
+	gulp.watch(paths.src.root + '/**/*.html', ['html']);
+	gulp.watch(paths.src.sass, ['sass']);
+	gulp.watch(paths.src.css + '/**/*', ['css']);
+	gulp.watch(paths.src.js + '/**/*', ['js']);
+	//gulp.watch(paths.src.img + '/**/*', ['img']);
 });
 
 
